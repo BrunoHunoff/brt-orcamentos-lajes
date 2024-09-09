84de869797cbbcc8f2ea60b7f86b4da5c25cb895
BrunoHunoff <brunohunoff@gmail.com>
2024-09-09 10:49:17 -0300

    ajuste temporario de campos dropdown

--- src/components/OrcamentoDataHeader/orcamentoDataHedaer.jsx
+++ src/components/OrcamentoDataHeader/orcamentoDataHedaer.jsx
@@ -1,22 +1,62 @@
-import Input from '../Input/input'
-import './orcamentoDataHeader.css'
+import Input from "../Input/input";
+import "./orcamentoDataHeader.css";
 
 function OrcamentoDataHeader() {
-    return (
-        <div className="orcamentoDataHeader">
-            <div className="number-row">
-                <Input labelFor= '' labelText='Nº Orçamento' inputType='text' placeHolder='00-000-0000' inputId='orcamentoNumber' width='140px' textAlign='center'/>
-            </div>
+  return (
+    <div className="orcamentoDataHeader">
+      <div className="number-row">
+        <Input
+          labelFor=""
+          labelText="Nº Orçamento"
+          inputType="text"
+          placeHolder="00-000-0000"
+          inputId="orcamentoNumber"
+          width="140px"
+          textAlign="center"
+        />
+      </div>
 
-            <div className="data-row">
-                <Input labelText='Cliente' inputType='text' placeHolder='Selecione' inputId='orcamentoClient' width='220px'/>
-                <Input labelText='Cidade' inputType='text' placeHolder='Selecione' inputId='orcamentoCity' width='220px'/>
-                <Input labelText='UF' inputType='text' placeHolder='UF' inputId='orcamentoUf' width='60'px/>
-                <Input labelText='Tipo Frete' inputType='text' placeHolder='Selecione' inputId='orcamentoFreightType' width='120px'/>
-                <Input labelText='Valor Carga' inputType='text' placeHolder='R$' inputId='freightPrice' width='200px'/>
-            </div>
+      <div className="data-row">
+        <div className="select-container">
+          <label htmlFor="clientes">Cliente</label>
+          <select id="clientes" className="clientes-select">
+            <option value="opcao1">Cliente 1</option>
+            <option value="opcao2">Cliente 2</option>
+            <option value="opcao3">Cliente 3</option>
+          </select>
         </div>
-    )
+        <Input
+          labelText="Cidade"
+          inputType="text"
+          placeHolder="Selecione"
+          inputId="orcamentoCity"
+          width="220px"
+        />
+        <Input
+          labelText="UF"
+          inputType="text"
+          placeHolder="UF"
+          inputId="orcamentoUf"
+          width="60"
+          px
+        />
+        <div className="select-container">
+          <label htmlFor="frete">Frete</label>
+          <select id="frete" className="frete-select">
+            <option value="opcao1">FOB</option>
+            <option value="opcao2">CIF</option>
+          </select>
+        </div>
+        <Input
+          labelText="Valor Carga"
+          inputType="text"
+          placeHolder="R$"
+          inputId="freightPrice"
+          width="200px"
+        />
+      </div>
+    </div>
+  );
 }
 
-export default OrcamentoDataHeader
+export default OrcamentoDataHeader;
