34607b31ec98b759a7772a794fc8c833fa9832d7
BrunoHunoff <brunohunoff@gmail.com>
2024-10-09 17:09:08 -0300

    Calcula footage e cost ao entrar na página

--- src/pages/Calculo/calculo.jsx
+++ src/pages/Calculo/calculo.jsx
@@ -1,34 +1,41 @@
-import './calculo.css'
+import "./calculo.css";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import Header from "../../components/Header/header";
+import Sidebar from "../../components/Sidebar/sidebar";
+import CalculoTable from "../../components/CalculoTable/calculoTable";
+import ResumoTable from "../../components/ResumoTable/resumoTable";
+import NavRow from "../../components/NavRow/navRow";
 
-import Header from "../../components/Header/header"
-import Sidebar from "../../components/Sidebar/sidebar"
-import CalculoTable from '../../components/CalculoTable/calculoTable'
-import ResumoTable from '../../components/ResumoTable/resumoTable'
-import NavRow from '../../components/NavRow/navRow'
+function Calculo({}) {
+  const location = useLocation();
+  const { dataRows } = location.state || { dataRows: [] };
 
-function Calculo( { dataRows, lajes } ) {
-    let footage = 0
-    let cost = 0
+  let footage = 0;
+  let cost = 0;
 
-    dataRows.forEach(row => {
-        footage += (row.data[1] * row.data[5] * row.data[6]);
-        cost += (footage)
-    });
+  useEffect(() => {
+    dataRows.forEach((row) => {
+        footage += row.data[1] * row.data[5] * row.data[6];
+        cost += footage * row.selectedLaje.price;
+      });
 
-    return (
-        <div className="calculo">
-            <Sidebar/>
+  }, []);
 
-            <div className="content">
-                <Header pageTitle = 'Calculo' userName = 'Bruno Hunoff' />
-                <div className='tables-container'>
-                    <CalculoTable/>
-                    <ResumoTable/>
-                </div>
-                <NavRow showVoltar={true}/>
-            </div>
+  return (
+    <div className="calculo">
+      <Sidebar />
+
+      <div className="content">
+        <Header pageTitle="Calculo" userName="Bruno Hunoff" />
+        <div className="tables-container">
+          <CalculoTable />
+          <ResumoTable />
         </div>
-    )
+        <NavRow showVoltar={true} />
+      </div>
+    </div>
+  );
 }
 
-export default Calculo
+export default Calculo;
