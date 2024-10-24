c1a3e3c4fd5123fb3287d61a6fe1ec5e0e66d6f9
BrunoHunoff <brunohunoff@gmail.com>
2024-10-24 16:32:55 -0300

    Cria HTML estático

--- src/pages/Proposta/proposta.jsx
+++ src/pages/Proposta/proposta.jsx
@@ -35,7 +35,9 @@ function Proposta() {
 				 <p style="margin-bottom: 30px">Prezados(as)</p>
 				 <p style="">Conforme solicitação de V.Sa. apresentamos nossa proposta para fornecimento de <strong>LAJE ALVEOLAR PROTENDIDA</strong>.</p>
 			 </div>
-			 <h3 style="color:blue; font-size: 20px; width: 100%; text-align: baseline;">1 - Especificação</h3>
+			 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline;">
+			 1 - Especificação
+			 </h3>
 			 <p style="margin-block: 40px; font-size: 16px">Consiste no fornecimento de materiais em obediência às normas relativas ao projeto de montagem e controle de qualidade definidas pela ABNT. <br>
 				 <br>Nesta proposta consideramos o fornecimento de peças para oxecução da obra com as seguintes dimensões:
 			 </p>
@@ -65,16 +67,16 @@ function Proposta() {
 	
 	<!-- PAGINA 2 -->
 	 <div style="height: 1150px; margin-top: 120px; margin-bottom: 200px;background: ; flex: 0 0 auto;">
-	 <div style="font-weight: bold; font-size: 12px; margin-bottom: 6mm;">
-		 RELAÇÃO DE PRODUTOS
-	 </div>
-	 <table style="width: 100%; border-collapse: collapse; margin-bottom: 10mm; font-size: 9px;">
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 2 - Relação de produtos
+	 </h3>
+	 <table style="width: 100%; border-collapse: collapse; margin-bottom: 10mm; font-size: 18px;">
 		 <thead>
 			 <tr>
 				 <th style="text-align: left; padding: 3mm; border: 1px solid #000;">Item</th>
 				 <th style="text-align: left; padding: 3mm; border: 1px solid #000;">Descrição</th>
-				 <th style="text-align: right; padding: 3mm; border: 1px solid #000;">Quantidade</th>
-				 <th style="text-align: right; padding: 3mm; border: 1px solid #000;">Unidade</th>
+				 <th style="text-align: right; padding: 3mm; border: 1px solid #000;">Qtd.</th>
+				 <th style="text-align: right; padding: 3mm; border: 1px solid #000;">Uni.</th>
 				 <th style="text-align: right; padding: 3mm; border: 1px solid #000;">Valor</th>
 			 </tr>
 		 </thead>
@@ -95,31 +97,131 @@ function Proposta() {
 			 </tr>
 		 </tbody>
 	 </table>
-	 <!-- Valor total -->
-	 <div style="text-align: right; font-weight: bold; font-size: 12px; margin-bottom: 10mm;">
-		 Valor Total: R$ 154.932,25
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 3 - Assistência
+	 </h3>
+		<p style="font-weight: bold; font-size: 18px";>
+		A CONTRATADA oferece serviços de assessoria técnica como: Visitas In-loco, Sugestões de
+Projetos, Soluções de Aplicação das Lajes Alveolares Protendidas, entre outros.
+		</p>
 	 </div>
-	</div>
-	 <!-- Condições de pagamento -->
-	 <div style="font-weight: bold; font-size: 12px; margin-bottom: 6mm;">
-		 CONDIÇÕES DE PAGAMENTO / DADOS BANCÁRIOS
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 4 - Garantia
+	 </h3>
+		<p style="font-weight: bold; font-size: 18px;">
+		Garantia por 5 (cinco) anos de acordo com o Código Civil Brasileiro, para imperfeições relativas aos
+elementos pré-fabricados de concreto, desde que exclusivamente em conseqüência de defeitos de
+fabricação com base no projeto aprovado pelo CONTRATANTE e CONTRATADA.
+		</p>
 	 </div>
-	 <p style="font-size: 10px; margin: 4mm 0;">A combinar</p>
-	 <!-- Seção de garantia -->
-	 <div style="font-weight: bold; font-size: 12px; margin-bottom: 6mm;">
-		 GARANTIA
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 5.1 - Valor Total Lajes
+	 </h3>
+		<div style="border: 1px solid black; padding: 12px; display: flex; gap: 24px; font-size: 22px; font-weight: bold;">
+			<span>R$100.000,00</span>
+			<span>Cem mil Reais</span>
+		</div>
 	 </div>
-	 <p style="font-size: 10px; margin: 4mm 0;">Garantia por 5 (cinco) anos de acordo com o Código Civil Brasileiro, para imperfeições relativas aos elementos pré-fabricados de concreto, desde que exclusivamente em consequência de defeitos de fabricação com base no projeto aprovado pelo CONTRATANTE e CONTRATADA.</p>
-	 <!-- Assinatura -->
-	 <div style="margin-top: 20mm; font-size: 10px;">
-		 <p style="margin: 4mm 0;">_______________________________________</p>
-		 <p style="margin: 4mm 0;">Assinatura do Contratante</p>
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 5.2 - Valor Total Frete
+	 </h3>
+		<p style="font-weight: bold; font-size: 18px"> 10 Fretes - Curitiba/Pr (FOB) </p>
 	 </div>
-	 <!-- Responsáveis -->
-	 <div style="margin-top: 15mm;">
-		 <p style="font-weight: bold; font-size: 10px; margin: 4mm 0;">HEVERTON CZARNIK</p>
-		 <p style="font-weight: bold; font-size: 10px; margin: 4mm 0;">DIREÇÃO GERAL</p>
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 6 - Condições de Pagamento
+	 </h3>
+		<p style="font-weight: bold; font-size: 18px"> A combinar </p>
 	 </div>
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 7 - Prazo de Entrega
+	 </h3>
+		<p style="font-weight: bold; font-size: 18px"> A combinar </p>
+	 </div>
+
+	 <div style="margin-bottom: 170px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 8 - Responsabilidade Técnica
+	 </h3>
+		<p style=" font-size: 18px; margin-bottom: 12px"> 
+		8.1 - O projeto estrutural, a Anotação de Responsabilidade Técnica (ART) bem como suas taxas, serão
+de responsabilidade da CONTRATADA. 
+</p>
+<p style=" font-size: 18px"> 
+		8.2 - Correrá por conta do Cliente todos os encargos e obrigações relativas à aprovação e execução de
+projetos e requerimentos de alvarás junto aos órgãos públicos competentes, como: Prefeituras Municipais,
+INSS, DNER, DNIT, entre os outros.
+</p>
+	 </div>
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 9 - Serviços de responsabilidade do Contratante
+	 </h3>
+		<p style=" font-size: 18px; margin-bottom: 12px"> 
+		9.1 - Retiradas dos Materiais na Unidade Fábril da CONTRATADA (Balsa Nova/PR), exceto quando frete CIF.
+</p>
+<p style=" font-size: 18px"> 
+		9.2 - Transporte, Estocagem em Obra e Montagem das Lajes
+</p>
+<p style=" font-size: 18px"> 
+		9.3 - Execução de quaisquer tipos de serviços "in loco", inclusive a equalização, chaveteamento e capeamento das lajes alveoares;
+</p>
+<p style=" font-size: 18px"> 
+		9.4 - Aprovar Projeto Junto à Contratada.
+</p>
+	 </div>
+
+	 <div style="margin-bottom: 30px">
+	 
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 10 - Local da Obra
+	 </h3>
+		<p style="font-weight: bold; font-size: 18px"> Obra a ser realizada no município de Curitiba/PR </p>
+	 </div>
+
+	 <h3 style="color:blue; font-size: 24px; width: 100%; text-align: baseline; margin-bottom: 24px">
+	 11 - Aceite de Proposta
+	 </h3>
+	 <div style="text-align: center">
+	 	<p style="font-size: 18px; margin-bottom: 48px"> Proposta válida por 05 (cinco) dias.</p>
+
+
+		<div style="display: flex; gap: 80px; justify-content: center; margin-bottom: 48px">
+		<span style="font-weight: bold; font-size: 18px"> De acordo: ____/____/________ </span>
+		<span style="font-weight: bold; font-size: 18px; text-align: center"> _______________________________________ <br> Assinatura Contratante </span>
+		</div>
+		</div>
+
+		<p style="font-weight: bold; font-size: 18px; margin-bottom: 24px;"> Transformo em Ordem de Compra este Documento nº: 01-182-1024 </p>
+
+		<p style="font-weight: bold; font-size: 18px; margin-bottom: 24px;"> O inicio ao procedimento interno de implantação de pedido e elaboração do projeto se
+dá a partir do aceite desta proposta e liberação do crédito. </p>
+
+		<p style="font-weight: bold; font-size: 18px; margin-bottom: 24px;"> FAVOR ENCAMINHAR ESTA PROPOSTA E COMPROVANTE DE DEPÓSITO NO EMAIL: lajes@sideralpremoldados.com.br </p>
+		
+	 </div>
+
+	 
 	</div>
 	`;
 
