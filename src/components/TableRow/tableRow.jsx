0a3ebfc7cef6eca199fe2ec218a5ab4549aac40c
BrunoHunoff <brunohunoff@gmail.com>
2024-10-29 20:53:40 -0300

    a

--- src/components/TableRow/tableRow.jsx
+++ src/components/TableRow/tableRow.jsx
@@ -4,47 +4,40 @@ import EditIcon from "../Icons/Edit";
 import FileIcon from "../Icons/FileIcon";
 import "./tableRow.css";
 
-
-function TableRow( { onDelete, data, onEdit, isCostumer }) {
-
+function TableRow({ onDelete, data, onEdit, isCostumer }) {
   const rowData = isCostumer
-    ? [
-        data.id,
-        data.name,
-        `${data.city}/${data.state}`,
-        data.cnpjCpf,
-      ]
-    : [
-        data.id,
-        data.costumerName,
-        data.footage,
-        `${data.city}/${data.state}`,
-      ];
+    ? [data.id, data.name, `${data.city}/${data.state}`, data.cnpjCpf]
+    : [data.id, data.costumerName, data.footage, `${data.city}/${data.state}`];
 
-  console.log(data)
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   function onEdit() {
-
-    navigate(`/orcamento/${rowData[0]}`)
+    navigate(`/orcamento/${rowData[0]}`);
   }
 
   return (
     <tr className="table-row">
-      {rowData.map ((item, index) => {
-        return <td key={index} className="table-data">{item}</td>
+      {rowData.map((item, index) => {
+        return (
+          <td key={index} className="table-data">
+            {item}
+          </td>
+        );
       })}
-       
+
       <td className="table-data actions-container">
-        <button className='action' onClick={() => onEdit(data[0])}>
-            <EditIcon/>
-        </button>
-        <button className='action'>
-          <DeleteIcon onClick={() => onDelete(data[0])} width='20px' height='20px'/>
+        <button className="action" onClick={() => onEdit(data[0])}>
+          <EditIcon />
         </button>
         <button className="action">
-            <FileIcon/>
+          <DeleteIcon
+            onClick={() => onDelete(data[0])}
+            width="20px"
+            height="20px"
+          />
+        </button>
+        <button className="action">
+          <FileIcon />
         </button>
       </td>
     </tr>
